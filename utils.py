a85f46b2f50229deec5d4a8a1e60c9b9b7f6b36b
jrudascas <jrudascas@gmail.com>
2018-09-11 17:44:21 -0500

    Changes

--- utils.py
+++ utils.py
@@ -146,9 +146,10 @@ def toFindStatisticDifference(x, y, outlier = None, measure='manwhitneyu', thres
     for comparator in range(x.shape[-1]):
         if measure == 'manwhitneyu':
             if outlier is None:
-                print(x[:,comparator])
-                print(y[:,comparator])
-                t, p = stats.mannwhitneyu(x[:,comparator], y[:, comparator])
+                if x[:,comparator] != y[:,comparator]:
+                    t, p = stats.mannwhitneyu(x[:,comparator], y[:, comparator])
+                else:
+                    p = 1
             elif np.isnan(outlier):
                 auxTemp1 = x[~np.isnan(x[:, comparator]), comparator]
                 auxTemp2 = y[~np.isnan(y[:, comparator]), comparator]
